e91bfd3c9f39dab4ec8441531d2b61d1e1c7718d
Jay Arredondo <jearredondo13@gmail.com>
2020-07-18 15:30:10 -0500

    started exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,12 +3,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+
+        if(!(boolean)session.getAttribute("user")) {
+            response.sendRedirect("/login");
+            return;
+        }
+        request.getRequestDispatcher("../webapp/WEB-INF/login.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -16,10 +23,15 @@ public class LoginServlet extends HttpServlet {
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
+        HttpSession session = request.getSession();
+
         if (validAttempt) {
+            session.setAttribute("user", true);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
         }
+
+
     }
 }
